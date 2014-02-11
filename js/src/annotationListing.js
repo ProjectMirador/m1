3081c2569f40b308df51126e7d5886f25f311948
aeschylus <scipioaffricanus@gmail.com>
2014-02-11 14:20:18 -0800

    basic filtering added, still need to fix bottom panel behaviour and add tests.

--- js/src/annotationListing.js
+++ js/src/annotationListing.js
@@ -1,49 +1,50 @@
 (function($) {
 
-  $.AnnotationListing = function(options) {
-    jQuery.extend(true, this, {
-      element:null,
-      id: null,
-      title: null,
-      content: null
-    }, options);
+    $.AnnotationListing = function(options) {
+        jQuery.extend(true, this, {
+            element:null,
+            id: null,
+            title: null,
+            content: null,
+            type: null
+        }, options);
 
-    this.create();
-  };
+        this.create();
+    };
 
-  $.AnnotationListing.prototype = {
+    $.AnnotationListing.prototype = {
 
-    create: function() {
-      this.render();
-    },
+        create: function() {
+            this.render();
+        },
 
-    render: function() {
-      templateData = {
-        id: this.id,
-        title: this.title,
-        content: this.content
-      };
+        render: function() {
+            templateData = {
+                id: this.id,
+                title: this.title,
+                content: this.content
+            };
 
-      this.element = jQuery($.Templates.imageView.annotationListing(templateData));
-      this.parent.listShell.append(this.element);
-      this.bindEvents();
-    },
+            this.element = jQuery($.Templates.imageView.annotationListing(templateData));
+            this.parent.listShell.append(this.element);
+            this.bindEvents();
+        },
 
-    bindEvents: function() {
-      var _this = this;
-      this.element.on('click', function() { _this.parent.parent.set('selectedAnnotation', _this.id, 'listing');} );
-      this.element.on('mouseover', function() { _this.parent.parent.set('hoveredAnnotation', _this.id, 'listing');} );
-      this.element.on('mouseleave', function() { _this.parent.parent.set('hoveredAnnotation', null, 'listing');} );
-    },
+        bindEvents: function() {
+            var _this = this;
+            this.element.on('click', function() { _this.parent.parent.set('selectedAnnotation', _this.id, 'listing'); } );
+            this.element.on('mouseover', function() { _this.parent.parent.set('hoveredAnnotation', _this.id, 'listing'); } );
+            this.element.on('mouseleave', function() { _this.parent.parent.set('hoveredAnnotation', null, 'listing'); } );
+        },
 
-    show: function() {
-      this.element.fadeIn();
-    },
+        show: function() {
+            this.element.fadeIn();
+        },
 
-    hide: function() {
-      this.element.fadeOut();
-    }
+        hide: function() {
+            this.element.fadeOut();
+        }
 
-  };
+    };
 
 }(Mirador));
